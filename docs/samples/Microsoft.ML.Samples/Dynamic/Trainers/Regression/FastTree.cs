1b1d69585b82414b12b8034a33d28529e2f2ddc4
Shahab Moradi <shmoradi@microsoft.com>
2019-03-15 11:12:34 -0700

    Added predictions and evaluation

--- docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/Regression/FastTree.cs
+++ docs/samples/Microsoft.ML.Samples/Dynamic/Trainers/Regression/FastTree.cs
@@ -16,22 +16,57 @@ namespace Microsoft.ML.Samples.Dynamic.Trainers.Regression
             // Setting the seed to a fixed number in this example to make outputs deterministic.
             var mlContext = new MLContext(seed: 0);
 
-            // Create a list of training examples.
+            // Create a list of training examples
             var examples = GenerateRandomDataPoints(1000);
 
             // Convert the examples list to an IDataView object, which is consumable by ML.NET API.
-            var data = mlContext.Data.LoadFromEnumerable(examples);
+            var trainingData = mlContext.Data.LoadFromEnumerable(examples);
 
             // Define the trainer.
-            var pipeline = mlContext.BinaryClassification.Trainers.FastTree();
+            var pipeline = mlContext.Regression.Trainers.FastTree();
 
             // Train the model.
-            var model = pipeline.Fit(data);
+            var model = pipeline.Fit(trainingData);
+
+            // Create testing examples. Use different random seed to make it
+            // different from training data.
+            var testingData = mlContext.Data.LoadFromEnumerable(
+                                    GenerateRandomDataPoints(500, seed:123));
+
+            var predictions = model.Transform(testingData);
+
+            // Look at 10 predictions
+            var trueLabels = predictions.GetColumn<float>("Label").Take(10).ToArray();
+            var predictedLabels = predictions.GetColumn<float>("Score").Take(10).ToArray();
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine($"Label: {trueLabels[i]:F3}, Prediction: {predictedLabels[i]:F3}");
+
+            // Expected output:
+            //   Label: 0.985, Prediction: 0.938
+            //   Label: 0.155, Prediction: 0.131
+            //   Label: 0.515, Prediction: 0.517
+            //   Label: 0.566, Prediction: 0.519
+            //   Label: 0.096, Prediction: 0.089
+            //   Label: 0.061, Prediction: 0.050
+            //   Label: 0.078, Prediction: 0.073
+            //   Label: 0.509, Prediction: 0.438
+            //   Label: 0.549, Prediction: 0.558
+            //   Label: 0.721, Prediction: 0.494
+
+            // Evaluate the overall metrics
+            var metrics = mlContext.Regression.Evaluate(predictions);
+            SamplesUtils.ConsoleUtils.PrintMetrics(metrics);
+
+            // Expected output:
+            //   Mean Absolute Error: 0.05
+            //   Mean Squared Error: 0.00
+            //   Root Mean Squared Error: 0.06
+            //   RSquared: 0.95
         }
 
-        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count)
+        private static IEnumerable<DataPoint> GenerateRandomDataPoints(int count, int seed=0)
         {
-            var random = new Random(0);
+            var random = new Random(seed);
             float randomFloat() => (float)random.NextDouble();
             for (int i = 0; i < count; i++)
             {
